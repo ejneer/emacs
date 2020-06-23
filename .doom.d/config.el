2789453076d509401400457e5bd3570bdbc655d3
Eric Neer <ericjneer@gmail.com>
2020-06-22 22:09:13 -0700

    update user info, enable treemacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Eric Neer"
+      user-mail-address "ericjneer@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -52,3 +52,5 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
