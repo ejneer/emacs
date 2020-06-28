212443efe6cbdbb5fda29e0d9b7738bce8346527
Eric Neer <ericjneer@gmail.com>
2020-06-28 09:18:09 -0700

    add python, pdftools settings

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -54,3 +54,25 @@
 ;; they are implemented.
 
 (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (org-noter-pdftools org-noter))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;  https://github.com/fuxialexander/org-pdftools
+(use-package org-pdftools
+  :hook (org-load . org-pdftools-setup-link))
+
+(use-package org-noter-pdftools
+  :after org-noter
+  :config
+  (with-eval-after-load 'pdf-annot
+   (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
